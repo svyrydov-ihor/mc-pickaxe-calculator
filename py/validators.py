6cd30e916eb1cc757a1b2556c33ba106294c3932
svyrydov-ihor <148194827+svyrydov-ihor@users.noreply.github.com>
2025-06-21 22:33:18 +0300

    Input validation added

---
+++ py/validators.py
@@ -0,0 +1,44 @@
+from pyscript import document
+
+def validate_durability() -> int:
+    durability_in_element = document.querySelector("#durability_input")
+    try:
+        durability = int(durability_in_element.value)
+        if durability <= 0:
+            raise ValueError
+        return durability
+    except ValueError:
+        raise ValueError("Durability must be a positive integer")
+
+def validate_unbreaking_level() -> int:
+    unbreaking_level_element = document.querySelector("#unbreaking_input")
+    try:
+        unbreaking_level = int(unbreaking_level_element.value)
+        if unbreaking_level not in [1, 2, 3]:
+            raise ValueError
+        return unbreaking_level
+    except ValueError:
+        raise ValueError("Unbreaking level must be a positive integer from 0 and 3")
+
+def validate_blocks() -> int:
+    blocks_input_element = document.querySelector("#blocks_input")
+    try:
+        blocks = int(blocks_input_element.value)
+        if blocks <= 0:
+            raise ValueError
+        return blocks
+    except ValueError:
+        raise ValueError("Blocks number must be a positive integer")
+
+def validate_num_of_experiments() -> int:
+    default_num = 5000
+    num_of_experiments_element = document.querySelector("#num_of_experiments_input")
+    if num_of_experiments_element.value in [None, ""]:
+        return default_num
+    try:
+        num_of_experiments = int(num_of_experiments_element.value)
+        if num_of_experiments <= 0:
+            raise ValueError
+        return num_of_experiments
+    except ValueError:
+        raise ValueError("Number of experiments must be a positive integer")
